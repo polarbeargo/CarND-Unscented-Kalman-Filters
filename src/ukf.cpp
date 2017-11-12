22aeda0eca42ce0252dbfab77c8cd0aaf546dc22
ali <bow1226@gmail.com>
2017-11-12 15:02:54 +0800

    Generate sigma point and UKF Augmentation
    Generate sigma point and UKF Augmentation

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -52,6 +52,24 @@ UKF::UKF()
 
   Hint: one or more values initialized above might be wildly off...
   */
+
+  // Set initialize to false as default
+  is_initialized_ = false;
+
+  // Time when the state is true, in us
+  time_us_ = 0.0;
+
+  // Set state dimension
+  n_x_ = 5;
+
+  // Define spreading parameter
+  lambda_ = 3 - n_x_;
+
+  // Process noise standard deviation longitudinal acceleration in m/s^2
+  std_a_ = 0.2;
+
+  // set augmented dimension
+  n_aug_ = 7;
 }
 
 UKF::~UKF() {}
@@ -83,6 +101,56 @@ void UKF::Prediction(double delta_t)
   Complete this function! Estimate the object's location. Modify the state
   vector, x_. Predict sigma points, the state, and the state covariance matrix.
   */
+
+  // Create sigma point matrix
+  MatrixXd Xsig = MatrixXd(n_x_, 2 * n_x_ + 1);
+
+  // Calculate square root of P
+  MatrixXd A = P_.llt().matrixL();
+
+  // Set first column of sigma point matrix
+  Xsig.col(0) = x_;
+
+  // Set remaining sigma points
+  for (int i = 0; i < n_x_; i++)
+  {
+    Xsig.col(i + 1) = x_ + sqrt(lambda_ + n_x_) * A.col(i);
+    Xsig.col(i + 1 + n_x_) = x_ - sqrt(lambda_ + n_x_) * A.col(i);
+  }
+
+  // Create augmented mean vector
+  VectorXd x_aug = VectorXd(7);
+
+  // Create augmented state covariance
+  MatrixXd P_aug = MatrixXd(7, 7);
+
+  // Create sigma point matrix
+  MatrixXd Xsig_aug = MatrixXd(n_aug_, 2 * n_aug_ + 1);
+
+  // Define spreading parameter
+  lambda_ = 3 - n_aug_;
+
+  // Create augmented mean state
+  x_aug.head(5) = x_;
+  x_aug(5) = 0;
+  x_aug(6) = 0;
+
+  // Create augmented covariance matrix
+  P_aug.fill(0.0);
+  P_aug.topLeftCorner(5, 5) = P_;
+  P_aug(5, 5) = std_a_ * std_a_;
+  P_aug(6, 6) = std_yawdd_ * std_yawdd_;
+
+  // Create square root matrix
+  MatrixXd L = P_aug.llt().matrixL();
+
+  // Create augmented sigma points
+  Xsig_aug.col(0) = x_aug;
+  for (int i = 0; i < n_aug_; i++)
+  {
+    Xsig_aug.col(i + 1) = x_aug + sqrt(lambda_ + n_aug_) * L.col(i);
+    Xsig_aug.col(i + 1 + n_aug_) = x_aug - sqrt(lambda_ + n_aug_) * L.col(i);
+  }
 }
 
 /**
