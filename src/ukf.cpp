e9ea3b5f0cf75689072e95a12c12a44919ea5c41
ali <bow1226@gmail.com>
2017-11-19 14:51:42 +0800

    Tune these values std_a_ and std_yawdd_
    Tune these values std_a_ and std_yawdd_ as reviewer's suggestion

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -25,10 +25,10 @@ UKF::UKF()
   P_ = MatrixXd(5, 5);
 
   // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 30;
+  std_a_ = 1.5;
 
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 30;
+  std_yawdd_ = 0.5;
 
   // Laser measurement noise standard deviation position1 in m
   std_laspx_ = 0.15;
@@ -64,9 +64,6 @@ UKF::UKF()
 
   // Define spreading parameter
   lambda_ = 3 - n_x_;
-
-  // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 1.5;
 
   // set augmented dimension
   n_aug_ = 7;
